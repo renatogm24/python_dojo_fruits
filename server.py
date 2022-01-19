9dcfdf0d29fec60dc85744301fa77d67d38b97b3
renatogm24 <renato.garay@pucp.pe>
2022-01-19 10:42:11 -0500

    Dojo fruits complete with python

--- server.py
+++ server.py
@@ -8,11 +8,14 @@ def index():
 @app.route('/checkout', methods=['POST'])         
 def checkout():
     print(request.form)
-    return render_template("checkout.html")
+    total = int(request.form["strawberry"]) + int(request.form["apple"]) +int(request.form["raspberry"]) 
+    print(f"Cobrando a {request.form['first_name']} por {total} frutas")
+    return render_template("checkout.html", data = request.form)
 
 @app.route('/fruits')         
 def fruits():
-    return render_template("fruits.html")
+    fruits = ["apple.png","blackberry.png","raspberry.png","strawberry.png"]
+    return render_template("fruits.html", fruits = fruits)
 
 if __name__=="__main__":   
     app.run(debug=True)    
